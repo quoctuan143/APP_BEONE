a4a98908ca28b6a2d210912c2df0db307c0bac31
nguyenquoctuan <quoctuan143@gmail.com>
2020-06-30 16:02:02 +0700

    Tuan Tao

---
+++ BEONE/BEONE/BEONE/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using BEONE.Models;
+
+namespace BEONE.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
